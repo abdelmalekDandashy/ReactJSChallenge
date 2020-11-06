b07ddbbdec9dcb8509f7a6ecf0af517e1619593c
abdelmalekDandashy <dandasheh5@gmail.com>
2020-11-06 19:38:16 +0200

    feature: final css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Mylogo from '../assets/images/EURISKO-MOBILITY.png'
-import './Header.css'
 function Header() {
     return (
 
         <div>
             <nav className="navbar navbar-light ">
        
-            <img src={Mylogo} width="190" height="100" class="d-inline-block align-top" alt=""/>
-            <h2 className='text-danger text-left '>EURISKO-FLIX</h2>
+            <img src={Mylogo} width="200px" height="100px"  alt=""/>
+            <h2 className='text-white  '>MOVIES DATABASE</h2>
         </nav>
         </div>
 
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -10,12 +10,13 @@ const MovieDetails = (props) => {
     let image = props.data.movieDetails.movieDetails.poster_path;
     console.log(props.data)
     return (
-        <div className='container'>
+        <div className='container' style={{ backgroundImage: `url(${base_URL}${image})` }} >
 
             
-            <div className='background' style={{ backgroundImage: `url(${base_URL}${image})` }}></div>
-            <h1 className='name'>{props.data.movieDetails.movieDetails.name} rating:   {props.data.movieDetails.movieDetails.vote_average}</h1>
-            <h3 className='desc'>{props.data.movieDetails.movieDetails.overview}</h3>
+            <div className=' background' style={{ backgroundImage: `url(${base_URL}${image})` }}></div>
+            <h1 className='content name'>{props.data.movieDetails.movieDetails.name} </h1>
+            <h3 className='content rating'>Rating: {props.data.movieDetails.movieDetails.vote_average} </h3>
+            <h4 className='content desc'>{props.data.movieDetails.movieDetails.overview}</h4>
 
         </div>
     );
